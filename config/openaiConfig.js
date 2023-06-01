9dc5a7e6bc693cdb8bbeb8ab73b01dd252568c16
Titouhi Omar <titouhiomar223@gmail.com>
2023-06-01 23:35:00 +0100

    Initial commit

---
+++ config/openaiConfig.js
@@ -0,0 +1,9 @@
+const {Configuration, OpenAIApi} = require('openai');
+require('dotenv').config()
+const configuration = new Configuration({
+    apiKey : process.env.OPEN_AI_KEY,
+})
+
+const openai = new OpenAIApi(Configuration)
+
+module.exports = openai
